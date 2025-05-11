bed2041ec403c48df5e8e5b839802908e6b07182
juntae02 <juntaepark02@gmail.com>
2025-05-11 22:22:40 +0900

    Ignore log directory

--- build/turtlebot4_beep/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/juntae02/rokey_ws/install/turtlebot4_beep'
--- build/turtlebot4_beep/share/turtlebot4_beep/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/juntae02/rokey_ws/build/turtlebot4_beep"
