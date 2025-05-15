8159c2fb6a1879569cda7216524a1ce22f2c9980
juntae02 <juntaepark02@gmail.com>
2025-05-15 10:30:35 +0900

    이미지 테스트

---
+++ src/for_git/image_test.py
@@ -0,0 +1,67 @@
+from ultralytics import YOLO
+import cv2
+
+# 학습된 모델 로드
+model = YOLO("/home/juntae02/Desktop/best.pt")
+
+# 카메라 스트림 열기 (카메라 인덱스 0, 필요 시 변경)
+cap = cv2.VideoCapture(2)
+
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+    
+    # 160x160으로 리사이징
+    frame_resized = cv2.resize(frame, (160, 160))
+    
+    # 프레임 전처리: 밝기/대비 조정
+    frame_resized = cv2.convertScaleAbs(frame_resized, alpha=1.2, beta=10)
+    
+    # 예측
+    results = model.predict(frame_resized, conf=0.05, iou=0.5, device=1)
+    
+    # 결과 시각화
+    annotated_frame = results[0].plot()
+    cv2.imshow("Fire Detection", annotated_frame)
+    
+    # 디버깅: 탐지된 객체의 Confidence 출력
+    for r in results:
+        for box in r.boxes:
+            print(f"Confidence: {box.conf.item():.3f}, Class: {box.cls.item()}")
+    
+    # 'q' 키로 종료
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+ 
+
+
+ 
+# import cv2
+
+# # 카메라 장치 열기 (기본 웹캠: 0번 장치)
+# cap = cv2.VideoCapture(2)
+
+# if not cap.isOpened():
+#     print("❌ 카메라를 열 수 없습니다.")
+#     exit()
+
+# print("✅ 카메라가 정상적으로 연결되었습니다. 'q'를 눌러 종료하세요.")
+
+# while True:
+#     ret, frame = cap.read()  # 프레임 읽기
+#     if not ret:
+#         print("⚠️ 프레임을 읽을 수 없습니다.")
+#         break
+
+#     cv2.imshow('Camera Test', frame)  # 화면에 프레임 보여주기
+
+#     if cv2.waitKey(1) & 0xFF == ord('q'):  # 'q'를 누르면 종료
+#         break
+
+# cap.release()
+# cv2.destroyAllWindows()
---
+++ src/rokey_pjt/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+package_name = 'rokey_pjt'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='juntae02',
+    maintainer_email='juntaepark02@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'capture_image = rokey_pjt.tb4_capture_image:main',
+            'cont_cap_image = rokey_pjt.tb4_cont_capture_image:main',
+            'det_obj = rokey_pjt.tb4_yolov8_obj_det:main',
+            'det_obj_thread = rokey_pjt.tb4_yolov8_obj_det_thread:main',
+            'det_obj_track = rokey_pjt.tb4_yolov8_obj_det_track:main',
+            'check_depth = rokey_pjt.depth_checker:main',
+            'check_depth_click = rokey_pjt.depth_checker_mouse_click:main',
+            'yolo_depth_checker = rokey_pjt.3_tb4_yolo_bbox_depth_checker:main',
+        ],
+    },
+)
